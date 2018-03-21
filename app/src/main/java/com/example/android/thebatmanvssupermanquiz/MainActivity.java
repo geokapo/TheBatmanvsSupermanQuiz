6d1cca35b1acc6b8584716fc9e43a9691cd9c1f8
geokapo <georgiakapodistria@gmail.com>
2018-03-21 21:15:31 +0200

    Some changes after review

--- app/src/main/java/com/example/android/thebatmanvssupermanquiz/MainActivity.java
+++ app/src/main/java/com/example/android/thebatmanvssupermanquiz/MainActivity.java
@@ -12,17 +12,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int totalPoints;
-RadioButton first_questionAnswerTwo;
-CheckBox second_questionAnswerTwo;
-RadioButton third_questionAnswerOne;
-RadioButton fourth_questionAnswerOne;
-RadioButton fifth_questionAnswerOne;
-CheckBox sixth_questionAnswerOne;
-RadioButton seventh_questionAnswerSecond;
-
-
-
+     int totalPoints;
+    EditText first_questionAnswerTwo;
+    CheckBox second_questionAnswerTwo;
+    CheckBox second_questionAnswerOne;
+    RadioButton third_questionAnswerOne;
+    RadioButton fourth_questionAnswerOne;
+    RadioButton fifth_questionAnswerOne;
+    RadioButton sixth_questionAnswerOne;
+    RadioButton seventh_questionAnswerSecond;
 
 
     @Override
@@ -30,56 +28,93 @@ RadioButton seventh_questionAnswerSecond;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        first_questionAnswerTwo = (RadioButton) findViewById(R.id.superm);
-        second_questionAnswerTwo = (CheckBox) findViewById(R.id.Crusader);
-        third_questionAnswerOne = (RadioButton) findViewById(R.id.ManOfSteel_2);
-        fourth_questionAnswerOne = (RadioButton) findViewById(R.id.Superman);
-        fifth_questionAnswerOne = (RadioButton) findViewById(R.id.Batman_2);
-        sixth_questionAnswerOne = (CheckBox) findViewById(R.id.ZackSnyder);
-        seventh_questionAnswerSecond = (RadioButton) findViewById(R.id.Isfalse);
+        first_questionAnswerTwo = findViewById(R.id.Superm);
+        second_questionAnswerTwo = findViewById(R.id.Penguin);
+        second_questionAnswerOne = findViewById(R.id.Joker);
+        third_questionAnswerOne = findViewById(R.id.ManOfSteel_2);
+        fourth_questionAnswerOne = findViewById(R.id.Superman);
+        fifth_questionAnswerOne = findViewById(R.id.Batman_2);
+        sixth_questionAnswerOne = findViewById(R.id.ZackSnyder);
+        seventh_questionAnswerSecond = findViewById(R.id.Isfalse);
     }
+
+
 
     /**
      * This method is activated when the Submit answers button is pressed
      */
 
-    public void submitAnswers(View view)
-    { EditText nameField = (EditText) findViewById(R.id.name);
-        Editable nameEditable = nameField.getText();
-        String name = nameEditable.toString();
+    public void submitAnswers(View view) {
 
-        if (first_questionAnswerTwo.isChecked()){
-            totalPoints +=1;
 
+        if(first_questionAnswerTwo.equals("Superman")) {
+            totalPoints += 1;
         }
 
-        if (second_questionAnswerTwo.isChecked()){
-            totalPoints +=1;
+        if (second_questionAnswerOne.isChecked() && (second_questionAnswerTwo.isChecked())){
+            totalPoints += 1;
         }
 
-        if (third_questionAnswerOne.isChecked()){
-            totalPoints +=1;
+        if (third_questionAnswerOne.isChecked()) {
+            totalPoints += 1;
         }
 
-        if (fourth_questionAnswerOne.isChecked()){
-            totalPoints +=1;
+        if (fourth_questionAnswerOne.isChecked()) {
+            totalPoints += 1;
         }
 
-        if (fifth_questionAnswerOne.isChecked()){
-            totalPoints +=1;
+        if (fifth_questionAnswerOne.isChecked()) {
+            totalPoints += 1;
         }
 
-        if (sixth_questionAnswerOne.isChecked()){
-            totalPoints +=1;
+        if (sixth_questionAnswerOne.isChecked()) {
+            totalPoints += 1;
         }
 
-        if (seventh_questionAnswerSecond.isChecked()){
-            totalPoints +=1;
+        if (seventh_questionAnswerSecond.isChecked()) {
+            totalPoints += 1;
+        }
+
+
+        //** * This method create results message*/
+
+
+        String Zero =  getResources().getString(R.string.toast0);
+
+        String lessPoints =  getResources().getString(R.string.toast1);
+
+        String Less = String.format(lessPoints, totalPoints);
+
+        String mediumPoints = getResources().getString(R.string.toast2);
+
+        String Medium = String.format(mediumPoints, totalPoints);
+
+        String highPoints =  getResources().getString(R.string.toast3);
+
+        String High = String.format(highPoints, totalPoints);
+
+
+        if(totalPoints == 0) {
+
+            Toast.makeText(this, Zero, Toast.LENGTH_LONG).show();
+
+        } else if(totalPoints == 1 || totalPoints == 2) {
+
+            Toast.makeText(this, Less, Toast.LENGTH_LONG).show();
+
+        } else if(totalPoints == 3 || totalPoints == 4) {
+
+            Toast.makeText(this, Medium, Toast.LENGTH_LONG).show();
+
+        } else if(totalPoints == 5 || totalPoints == 6) {
+
+            Toast.makeText(this, High, Toast.LENGTH_LONG).show();
+
         }
 
 
         Context context = getApplicationContext();
-        CharSequence text = "Hey " + name + "! \nYou got " + totalPoints + " points";
+        CharSequence text = "Hey " + "! \nYou got " + totalPoints + " points";
         text = text + "\n" + getString(R.string.you_have) + " " + totalPoints + " " + getString(R.string.correct_answers);
         int duration = Toast.LENGTH_LONG;
 
@@ -90,7 +125,7 @@ RadioButton seventh_questionAnswerSecond;
 
     }
 
-    }
+}
 
 
 

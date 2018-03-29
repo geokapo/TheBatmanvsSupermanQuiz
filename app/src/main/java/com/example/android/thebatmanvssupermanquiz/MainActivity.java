408f0ee20a82422cd891f5f432fd30f61b9d726d
geokapo <georgiakapodistria@gmail.com>
2018-03-29 22:46:47 +0300

    Some changes after review_2

--- app/src/main/java/com/example/android/thebatmanvssupermanquiz/MainActivity.java
+++ app/src/main/java/com/example/android/thebatmanvssupermanquiz/MainActivity.java
@@ -48,8 +48,7 @@ public class MainActivity extends AppCompatActivity {
     public int submitAnswers(View view) {
 
         EditText setResult = findViewById(R.id.Superm);
-        setResultFirst= 0;
-        if(setResult.getText().toString().equals("Superman")) setResultFirst = 1;
+        if(setResult.getText().toString().trim().equals("Superman")) setResultFirst += 1;
         else setResultFirst = 0;
 
 
@@ -71,44 +70,22 @@ public class MainActivity extends AppCompatActivity {
         if (seventh_questionAnswerSecond.isChecked()) totalPoints += 1;
 
 
+
         //** * This method create results message*/
 
 
-        String Zero =  getResources().getString(R.string.toast0);
-
-        String lessPoints =  getResources().getString(R.string.toast1);
-
-        String Less = String.format(lessPoints, totalPoints);
-
-        String mediumPoints = getResources().getString(R.string.toast2);
-
-        String Medium = String.format(mediumPoints, totalPoints);
-
-        String highPoints =  getResources().getString(R.string.toast3);
-
-        String High = String.format(highPoints, totalPoints);
-
-
-
-        if(totalPoints == 0) Toast.makeText(this, Zero, Toast.LENGTH_LONG).show();
-        else if(totalPoints == 1 || totalPoints == 2)
-            Toast.makeText(this, Less, Toast.LENGTH_LONG).show();
-        else if(totalPoints == 3 || totalPoints == 4)
-            Toast.makeText(this, Medium, Toast.LENGTH_LONG).show();
-        else if(totalPoints == 5 || totalPoints == 6)
-            Toast.makeText(this, High, Toast.LENGTH_LONG).show();
-
 
         Context context = getApplicationContext();
-        CharSequence text = "You got " + totalPoints + " points";
-        text = text + "\n" + getString(R.string.you_have) + " " + totalPoints + " " + getString(R.string.correct_answers);
+        CharSequence text = "You got " + totalPoints + setResultFirst+ " points";
+        text = text + "\n" + getString(R.string.you_have) + " " + totalPoints + setResultFirst+ " " + getString(R.string.correct_answers);
         int duration = Toast.LENGTH_LONG;
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
 
 
-
+        switch (setResultFirst = 0) {
+        }
     return totalPoints =0;
     }
 
